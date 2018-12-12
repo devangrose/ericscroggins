d764cf6be3efee99b787ebe76d84f223082ffe30
Devan Grose <devangrose@gmail.com>
2018-12-12 14:11:19 -0800

    started meeting planner

---
+++ src/views/MeetingPlanner/Card.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+
+};
+
+class CLASSNAME extends Component {
+  render(){
+    const { classes } = this.props;
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(CLASSNAME);
