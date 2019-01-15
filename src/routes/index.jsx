f6acc70e3009d528a6cd00d85f10687f213d287f
Devan Grose <devangrose@gmail.com>
2019-01-14 17:28:09 -0800

    removed unnecessary template pages

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,3 @@
-import AboutUsPage from "views/AboutUsPage/AboutUsPage.jsx";
-import BlogPostPage from "views/BlogPostPage/BlogPostPage.jsx";
-import BlogPostsPage from "views/BlogPostsPage/BlogPostsPage.jsx";
-import ComponentsPage from "views/ComponentsPage/ComponentsPage.jsx";
-import ContactUsPage from "views/ContactUsPage/ContactUsPage.jsx";
-import EcommercePage from "views/EcommercePage/EcommercePage.jsx";
-import LandingPage from "views/LandingPage/LandingPage.jsx";
-import LoginPage from "views/LoginPage/LoginPage.jsx";
-import PresentationPage from "views/PresentationPage/PresentationPage.jsx";
-import PricingPage from "views/PricingPage/PricingPage.jsx";
-import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
-import ProductPage from "views/ProductPage/ProductPage.jsx";
-import SectionsPage from "views/SectionsPage/SectionsPage.jsx";
-import ShoppingCartPage from "views/ShoppingCartPage/ShoppingCartPage.jsx";
-import SignupPage from "views/SignupPage/SignupPage.jsx";
 import Home from "views/Home/Home.js";
 import Contact from "views/Contact/Contact.js";
 import Speaker from "views/Speaker/Speaker.js";
@@ -23,7 +8,6 @@ import Resources from "views/Resources/Resources.js";
 import MeetingPlanner from "views/MeetingPlanner/MeetingPlanner.js";
 
 var indexRoutes = [
-  { path: "/signup-page", name: "SignupPage", component: SignupPage },
   { path: "/", name: "Home", component: Home },
   { path: "/contact", name: "Contact", component: Contact},
   { path: "/speaker", name: "Speaker", component: Speaker},
